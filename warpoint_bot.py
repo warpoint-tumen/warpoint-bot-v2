25f4218f750bbb9ba00626002dfeddef6da0a224
warpoint-tumen <tumen.cyngesambuev@gmail.com>
2025-07-30 05:20:19 +0300

    Add files via upload

--- warpoint_bot.py
+++ warpoint_bot.py
@@ -1,30 +1,28 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
-import logging
+import os
 
-# Включаем логирование
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+TOKEN = os.getenv("TOKEN")
 
-TOKEN = "8318731976:AAGRLByy52ordZtigWkkQ-Ux2Hf7x7AiLIE"
-
-# Команда /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот запущен. Добро пожаловать!")
+    await update.message.reply_text("Бот запущен!")
 
-# Команда /today
-async def today(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    now = datetime.now().strftime("%d.%m.%Y")
-    msg = f"Задачи на сегодня ({now}):\n- пример задачи 1\n- пример задача 2"
-    await update.message.reply_text(msg)
+async def send_daily_message():
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{now}] Ежедневное сообщение отправлено...")
 
-# Основной запуск
 def main():
-    app = ApplicationBuilder().token("8318731976:AAGRLByy52ordZtigWkkQ-Ux2Hf7x7AiLIE").build()
+    app = ApplicationBuilder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("today", today))
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(send_daily_message, 'cron', hour=8, minute=0)
+    scheduler.start()
+
+    print("Бот запущен...")
     app.run_polling()
 
 if __name__ == "__main__":
