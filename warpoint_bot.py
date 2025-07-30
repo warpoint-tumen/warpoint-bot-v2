3bd79a2ea720301ced768727bf4b931f66db2b4f
warpoint-tumen <tumen.cyngesambuev@gmail.com>
2025-07-30 04:51:28 +0300

    Add files via upload

--- warpoint_bot.py
+++ warpoint_bot.py
@@ -1,19 +1,24 @@
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from datetime import datetime
 
 TOKEN = "8318731976:AAGRLByy52ordZtigWkkQ-Ux2Hf7x7AiLIE"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я бот WarPoint.")
+
+async def today_tasks(update: Update, context: ContextTypes.DEFAULT_TYPE):
     today = datetime.now().strftime("%d.%m.%Y")
-    msg = f"Задачи на сегодня ({today}):"
+    msg = f"Задачи на сегодня ({today}):\n- Задача 1\n- Задача 2"
     await update.message.reply_text(msg)
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    print("Бот запущен... (заргужка)")
+    app.add_handler(CommandHandler("today", today_tasks))
     app.run_polling()
 
 if __name__ == "__main__":
+    print("Бот запущен...")
     main()
